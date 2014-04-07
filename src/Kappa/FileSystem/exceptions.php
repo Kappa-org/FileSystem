e5bcc1b5354b3516d6ebd9c0f865c2091bb71559
Budry <zarubaondra@gmail.com>
2014-04-07 12:57:00 +0200

    Added FileNotFound exception

---
+++ src/Kappa/FileSystem/exceptions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Kappa\FileSystem package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\FileSystem;
+
+/**
+ * Class FileNotFoundException
+ * @package Kappa\FileSystem
+ */
+class FileNotFoundException extends \LogicException
+{
+
+}
