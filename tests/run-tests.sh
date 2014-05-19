3edf6b29f8af7888b2e10cf46a5f8015098c89bb
Budry <zarubaondra@gmail.com>
2014-05-19 19:38:20 +0200

    Added test runner

---
+++ tests/run-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+composer=composer.json
+composer_backup=composer_backup.json
+
+trap rollback INT
+
+function backup()
+{
+	cp $composer $composer_backup
+}
+
+function rollback()
+{
+	echo "Rollback original composer.json and exit test"
+	mv $composer_backup $composer
+	composer update
+	exit 0;
+}
+
+backup
+variables="nette-2.1 nette-dev"
+for variable in $variables; do
+	export NETTE=$variable
+	php ./tests/prepare-composer.php
+	composer update
+	./vendor/bin/tester tests/ $@
+done
+rollback
